9a6c43e2336b8d67515a0d208d8cc5ae8adbeea2
StanleySathler <stanleysathlerpinto@gmail.com>
2019-09-27 19:46:44 -0300

    feat: move "index.js" to the root folder & config run scripts

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const path = require('path')
-const Lexer = require('./lexer')
-const FileReader = require('./reader')
+const Lexer = require('./src/lexer')
+const FileReader = require('./src/reader')
 
 /* Read the source code file */
-const filePath = path.resolve(__dirname, '../test/mocks/code-sample-01.pys')
+// const filePath = path.resolve(__dirname, '../test/mocks/code-sample-01.pys')
+const filePath = path.resolve(__dirname, process.argv[2])
 const fileReader = new FileReader(filePath)
 
 /* Run the Lexer analysis */
