503266474f75eb88c38ebccd101a4fec5b0f30e1
StanleySathler <stanleysathlerpinto@gmail.com>
2019-11-10 15:13:13 -0300

    feat(parser): add more rule parsers

--- src/parser/index.js
+++ src/parser/index.js
@@ -270,110 +270,267 @@ module.exports = class Parser {
    * ListaCmdLinha -> Cmd ListaCmdLinha | epsilon
    */
   parseListaCmdLinha() {
+    /* ListaCmdLinha -> Cmd ListaCmdLinha */
+    if (
+      this.__nextReadToken.getName() === TOKEN_NAMES.KW_IF ||
+      this.__nextReadToken.getName() === TOKEN_NAMES.KW_WHILE ||
+      this.__nextReadToken.getName() === TOKEN_NAMES.ID ||
+      this.__nextReadToken.getName() === TOKEN_NAMES.KW_WRITE
+    ) {
+      this.parseCmd()
+      this.parseListaCmdLinha()
+    }
 
+    /* ListaCmdLinha -> epsilon */
+    else if (
+      this.__nextReadToken.getName() === TOKEN_NAMES.KW_RETURN ||
+      this.__nextReadToken.getName() === TOKEN_NAMES.KW_END ||
+      this.__nextReadToken.getName() === TOKEN_NAMES.KW_ELSE
+    ) return
+
+    else
+      this.addError('parseListaCmdLinha')
   }
 
   /**
-   *
+   * Cmd -> CmdIF | CmdWhile | ID CmdAtribFunc | CmdWrite
    */
   parseCmd() {}
 
   /**
-   *
+   * CmdAtribFunc -> CmdAtribui | CmdFuncao
    */
   parseCmdAtribFunc() {}
 
   /**
-   *
+   * CmdIF -> if ( Expressao ) : ListaCmd CmdIFLinha
    */
-  parseCmdIF() {}
+  parseCmdIF() {
+    if (!this.eat(TOKEN_NAMES.KW_IF))
+      this.addError('parseCmdIF')
+
+    if (!this.eat(TOKEN_NAMES.OPN_RND_BRACKET))
+      this.addError('parseCmdIF')
+
+    this.parseExpressao()
+
+    if (!this.eat(TOKEN_NAMES.CLS_RND_BRACKET))
+      this.addError('parseCmdIF')
+
+    if (!this.eat(TOKEN_NAMES.COLON))
+      this.addError('parseCmdIF')
+
+    this.parseListaCmd()
+
+    this.parseCmdIFLinha()
+  }
 
   /**
-   *
+   * CmdIFLinha -> end ; | else : ListaCmd end ;
    */
-  parseCmdIF2() {}
+  parseCmdIFLinha() {
+    if (this.__nextReadToken.getName() === TOKEN_NAMES.KW_END) {
+      this.eat(TOKEN_NAMES.KW_END)
+
+      if (!this.eat(TOKEN_NAMES.SEMI_COLON))
+        this.addError('parseCmdIFLinha')
+    }
+
+    else if (this.__nextReadToken.getName() === TOKEN_NAMES.KW_ELSE) {
+      this.eat(TOKEN_NAMES.KW_ELSE)
+
+      if (!this.eat(TOKEN_NAMES.COLON))
+        this.addError('parseCmdIFLinha')
+
+      this.parseListaCmd()
+
+      if (!this.eat(TOKEN_NAMES.KW_END))
+        this.addError('parseCmdIFLinha')
+
+      if (!this.eat(TOKEN_NAMES.SEMI_COLON))
+        this.addError('parseCmdIFLinha')
+    }
+  }
 
   /**
-   *
+   * CmdWhile -> while ( Expressao ) : ListaCmd end ;
    */
-  parseCmdWhile() {}
+  parseCmdWhile() {
+    if (!this.eat(TOKEN_NAMES.KW_WHILE))
+      this.addError('parseCmdWhile')
+
+    if (!this.eat(TOKEN_NAMES.OPN_RND_BRACKET))
+      this.addError('parseCmdWhile')
+
+    this.parseExpressao()
+
+    if (!this.eat(TOKEN_NAMES.CLS_RND_BRACKET))
+      this.addError('parseCmdWhile')
+
+    if (!this.eat(TOKEN_NAMES.COLON))
+      this.addError('parseCmdWhile')
+
+    this.parseListaCmd()
+
+    if (!this.eat(TOKEN_NAMES.KW_END))
+      this.addError('parseCmdWhile')
+
+    if (!this.eat(TOKEN_NAMES.SEMI_COLON))
+      this.addError('parseCmdWhile')
+  }
 
   /**
-   *
+   * CmdWrite -> write ( Expressao ) ;
    */
-  parseCmdWrite() {}
+  parseCmdWrite() {
+    if (!this.eat(TOKEN_NAMES.KW_WRITE))
+      this.addError('parseCmdWrite')
+
+    if (!this.eat(TOKEN_NAMES.OPN_RND_BRACKET))
+      this.addError('parseCmdWrite')
+
+    this.parseExpressao()
+
+    if (!this.eat(TOKEN_NAMES.CLS_RND_BRACKET))
+      this.addError('parseCmdWrite')
+
+    if (!this.eat(TOKEN_NAMES.SEMI_COLON))
+      this.addError('parseCmdWrite')
+  }
 
   /**
-   *
+   * CmdAtribui -> = Expressao ;
    */
-  parseCmdAtribui() {}
+  parseCmdAtribui() {
+    if (!this.eat(TOKEN_NAMES.OP_EQ))
+      this.addError('parseCmdAtribui')
+
+    this.parseExpressao()
+
+    if (!this.eat(TOKEN_NAMES.SEMI_COLON))
+      this.addError('parseCmdAtribui')
+  }
 
   /**
-   *
+   * CmdFuncao -> ( RegexExp ) ;
    */
-  parseCmdFuncao() {}
+  parseCmdFuncao() {
+    if (!this.eat(TOKEN_NAMES.OPN_RND_BRACKET))
+      this.addError('parseCmdFuncao')
+
+    this.parseRegexExp()
+
+    if (!this.eat(TOKEN_NAMES.CLS_RND_BRACKET))
+      this.addError('parseCmdFuncao')
+
+    if (!this.eat(TOKEN_NAMES.SEMI_COLON))
+      this.addError('parseCmdFuncao')
+  }
 
   /**
-   *
+   * RegexExp -> Expressao RegexExpLinha | epsilon
    */
   parseRegexExp() {}
 
   /**
-   *
+   * RegexExpLinha -> , Expressao RegexExpLinha | epsilon
    */
-  parseRegexExp2() {}
+  parseRegexExpLinha() {
+    if (this.__nextReadToken.getName() === TOKEN_NAMES.COMMA) {
+      this.eat(TOKEN_NAMES.COMMA)
+
+      this.parseExpressao()
+
+      this.parseRegexExpLinha()
+    }
+
+    else if (this.__nextReadToken.getName() === TOKEN_NAMES.CLS_RND_BRACKET)
+      return
+
+    else
+      this.addError('parseRegexExpLinha')
+  }
 
   /**
-   *
+   * Expressao -> Exp1 ExpLinha
    */
-  parseExpressao() {}
+  parseExpressao() {
+    this.parseExp1()
+    this.parseExpLinha()
+  }
 
   /**
-   *
+   * ExpLinha -> or Exp1 ExpLinha | and Exp1 ExpLinha | epsilon
    */
   parseExpLinha() {}
 
   /**
-   *
+   * Exp1 -> Exp2 Exp1Linha
    */
-  parseExp1() {}
+  parseExp1() {
+    this.parseExp2()
+    this.parseExp1Linha()
+  }
 
   /**
-   *
+   * Exp1Linha -> < Exp2 Exp1Linha | <= Exp2 Exp1Linha | > Exp2 Exp1Linha |
+   * >= Exp2 Exp1Linha | == Exp2 Exp1Linha | != Exp2 Exp1Linha | epsilon
    */
   parseExp1Linha() {}
 
   /**
-   *
+   * Exp2 -> Exp3 Exp2Linha
    */
-  parseExp2() {}
+  parseExp2() {
+    this.parseExp3()
+    this.parseExp2Linha()
+  }
 
   /**
-   *
+   * Exp2Linha -> + Exp3 Exp2Linha | - Exp3 Exp2Linha | epsilon
    */
   parseExp2Linha() {}
 
   /**
-   *
+   * Exp4 Exp3Linha
    */
-  parseExp3() {}
+  parseExp3() {
+    this.parseExp4()
+    this.parseExp3Linha()
+  }
 
   /**
-   *
+   * Exp3Linha -> * Exp4 Exp3Linha | / Exp4 Exp3Linha | epsilon
    */
   parseExp3Linha() {}
 
   /**
-   *
+   * Exp4 -> ID Exp4Linha | ConstInteger | ConstDouble | ConstString |
+   * true | false | OpUnario Exp4 | ( Expressao )
    */
   parseExp4() {}
 
   /**
-   *
+   * Exp4Linha -> ( RegexExp ) | epsilon
    */
   parseExp4Linha() {}
+
+  /**
+   * OpUnario -> - | !
+   */
+  parseOpUnario() {
+    if (this.__nextReadToken.getName() === TOKEN_NAMES.OP_NGT)
+      this.eat(TOKEN_NAMES.OP_NGT)
+
+    else if (this.__nextReadToken.getName() === TOKEN_NAMES.OP_NOT)
+      this.eat(TOKEN_NAMES.OP_NOT)
+
+    else
+      this.addError('parseOpUnario')
+  }
 
   addError(expected) {
     /* @todo: implement */
   }
 }
+
