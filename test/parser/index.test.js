17f7577f94162e6cee6e011ca4b7dc3961a55093
StanleySathler <stanleysathlerpinto@gmail.com>
2019-12-05 00:02:28 -0300

    test(parser): add invalid code sample for testing purposes

--- test/parser/index.test.js
+++ test/parser/index.test.js
@@ -1,26 +1,46 @@
 const path = require('path')
 const FileReader = require('../../src/reader')
 const Lexer = require('../../src/lexer')
-const Token = require('../../src/token')
 const Parser = require('../../src/parser')
-const TOKEN_NAMES = require('../../src/token/names')
 
 describe('Given a Parser', () => {
-  describe('and it reads a valid code sample', () => {
+  describe.skip('and it reads a valid code sample', () => {
     let filePath
     let fileReader
     let lexer
     let parser
 
     beforeEach(() => {
-      filePath = path.resolve(__dirname, '../mocks/code-sample-01.pys')
+      filePath = path.resolve(__dirname, '../mocks/parser/valid-code-sample-01.pys')
       fileReader = new FileReader(filePath)
       lexer = new Lexer(fileReader)
       parser = new Parser(lexer)
+      parser.parse()
     })
 
     it('should NOT print any error', () => {
       const expected = 0
+      const actual = parser.__errors.length
+      expect(actual).toEqual(expected)
+    })
+  })
+
+  describe('and it reads an invalid code sample', () => {
+    let filePath
+    let fileReader
+    let lexer
+    let parser
+
+    beforeEach(() => {
+      filePath = path.resolve(__dirname, '../mocks/parser/invalid-code-sample-01.pys')
+      fileReader = new FileReader(filePath)
+      lexer = new Lexer(fileReader)
+      parser = new Parser(lexer)
+      parser.parse()
+    })
+
+    it('should identify all the errors', () => {
+      const expected = 3
       const actual = parser.__errors.length
       expect(actual).toEqual(expected)
     })
