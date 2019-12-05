fd3a2d85e782492012f536a438102896f4cb1600
StanleySathler <stanleysathlerpinto@gmail.com>
2019-12-05 00:02:53 -0300

    chore: change debugging option to test the parser

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const Table = require('cli-table')
 const path = require('path')
 const Lexer = require('./src/lexer')
 const FileReader = require('./src/reader')
@@ -6,6 +5,7 @@ const TOKEN_NAMES = require('./src/token/names')
 const TokensLogger = require('./src/logger/tokens')
 const SymbolTableLogger = require('./src/logger/symbol-table')
 const ErrorsLogger = require('./src/logger/errors')
+const Parser = require('./src/parser')
 
 /* Loggers */
 const tokensLogger = new TokensLogger()
@@ -19,31 +19,35 @@ const fileReader = new FileReader(filePath)
 /* Instantiate the Lexer */
 const lexer = new Lexer(fileReader)
 
-/* Build table w/ tokens */
-let token
-do {
-  token = lexer.nextToken()
-  tokensLogger.push(token)
-} while (token.getName() !== TOKEN_NAMES.EOF)
+// /* Build table w/ tokens */
+// let token
+// do {
+//   token = lexer.nextToken()
+//   tokensLogger.push(token)
+// } while (token.getName() !== TOKEN_NAMES.EOF)
 
-/* Build table w/ Symbol Table */
-const symbols = lexer.getSymbolTable().getSymbols()
-Object.keys(symbols).forEach(symbolLexem => {
-  const token = symbols[symbolLexem]
-  symbolTableLogger.push(token)
-})
+// /* Build table w/ Symbol Table */
+// const symbols = lexer.getSymbolTable().getSymbols()
+// Object.keys(symbols).forEach(symbolLexem => {
+//   const token = symbols[symbolLexem]
+//   symbolTableLogger.push(token)
+// })
 
-/* Build the errors table */
+// /* Build the errors table */
 
-if (lexer.getErrors().length) {
-  lexer.getErrors().map(error => {
-    errorsLogger.push(error)
-  })
-} else {
-  errorsLogger.push('No errors were found!')
-}
+// if (lexer.getErrors().length) {
+//   lexer.getErrors().map(error => {
+//     errorsLogger.push(error)
+//   })
+// } else {
+//   errorsLogger.push('No errors were found!')
+// }
 
-/* Print tables */
-tokensLogger.print()
-symbolTableLogger.print()
-errorsLogger.print()
+// /* Print tables */
+// tokensLogger.print()
+// symbolTableLogger.print()
+// errorsLogger.print()
+
+const parser = new Parser(lexer)
+
+parser.parse()
